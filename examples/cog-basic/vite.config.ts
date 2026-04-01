e09f4eb21ea0010c8c85024acc750c2ddff771e8
Roman Karavia <3082173+rkaravia@users.noreply.github.com>
2026-04-01 11:45:28 +0200

    Add netlify settings

--- examples/cog-basic/vite.config.ts
+++ examples/cog-basic/vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/deck.gl-raster/examples/cog-basic/",
   worker: { format: "es" },
   server: {
     port: 3000,
